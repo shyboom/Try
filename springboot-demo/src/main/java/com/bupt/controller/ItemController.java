e0d132f54de1b613545bb36918edeb6e1a3925fc
shyboom <35993052+shyboom@users.noreply.github.com>
2020-10-05 10:35:47 +0800

    Update ItemController.java

--- springboot-demo/src/main/java/com/bupt/controller/ItemController.java
+++ springboot-demo/src/main/java/com/bupt/controller/ItemController.java
@@ -31,6 +31,7 @@ public class ItemController {
 
     @RequestMapping(value = "/getItem/{id}", method = RequestMethod.GET)
     public String getItem(@PathVariable("id") Integer id) {
+        system.out.println("查询ID");
         return itemService.get(id).toString();
     }
     @RequestMapping(value = "/getItemWithName/{name}", method = RequestMethod.GET)
